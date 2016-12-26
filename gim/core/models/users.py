43d4a8eb4ce5391bb2a910473568f695588a516a
Stephane Angel (Twidi) <s.angel@twidi.com>
2016-12-26 03:46:08 +0100

    Import GenericForeignKey from the correct file

--- gim/core/models/users.py
+++ gim/core/models/users.py
@@ -10,7 +10,7 @@ __all__ = [
 import re
 
 from django.contrib.contenttypes.models import ContentType
-from django.contrib.contenttypes.generic import GenericForeignKey
+from django.contrib.contenttypes.fields import GenericForeignKey
 from django.contrib.auth.models import AbstractUser
 from django.db import models
 from django.utils.dateformat import format
