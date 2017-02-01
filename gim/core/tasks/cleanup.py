03975872e099ae95e64c350e44100247a1d1e036
Stephane Angel (Twidi) <s.angel@twidi.com>
2017-02-01 21:30:19 +0100

    Add a max_delete arg to jobs cleanup functions

--- gim/core/tasks/cleanup.py
+++ gim/core/tasks/cleanup.py
@@ -12,7 +12,7 @@ def _get_past_date(days):
     return (datetime.utcnow()-timedelta(days=days)).strftime('%Y-%m-%d')
 
 
-def clean_errors(log_step=1000, keep_days=7, batch_size=100):
+def clean_errors(log_step=1000, keep_days=7, batch_size=100, max_delete=None):
     """
     Remove from the Errors model all entries older than the given number of days
     """
@@ -20,7 +20,7 @@ def clean_errors(log_step=1000, keep_days=7, batch_size=100):
     max_date = _get_past_date(keep_days)
     done = 0
     errors = 0
-    logger.info('Deleting Error (max date = %s, total: %d)', max_date, count)
+    logger.info('Deleting Error (limit: %s, max-date: %s, total: %d)', max_delete, max_date, count)
     stop = False
     iterate = 0
     while not stop:
@@ -30,7 +30,7 @@ def clean_errors(log_step=1000, keep_days=7, batch_size=100):
         for instance in instances:
             iterate += 1
             if iterate and not iterate % log_step:
-                logger.info('Deleted %d on %d', done, count)
+                logger.info('Deleted %d on %d (limit %s)', done, count, max_delete)
             try:
                 if keep_days and instance.date.hget() >= max_date:
                     stop = True
@@ -41,11 +41,14 @@ def clean_errors(log_step=1000, keep_days=7, batch_size=100):
                 errors += 1
             else:
                 done += 1
+            if max_delete and iterate >= max_delete:
+                stop = True
+                break
 
-    logger.info('Total Error deleted: %d on %d, keep %d (including %d errors)', done, count, count - done, errors)
+    logger.info('Total Error deleted (limit %s): %d on %d, keep %d (including %d errors)', max_delete, done, count, count - done, errors)
 
 
-def clean_job_model(job_model, log_step=1000, keep_days=7, batch_size=100):
+def clean_job_model(job_model, log_step=1000, keep_days=7, batch_size=100, max_delete=None):
     """
     Remove from the given job model all entries older than the given number of days
     Also clear the success and errors list of all the queues (different priorities)
@@ -57,17 +60,17 @@ def clean_job_model(job_model, log_step=1000, keep_days=7, batch_size=100):
     done = 0
     errors = 0
     planned = set(sum([q.waiting.lmembers() + q.delayed.zmembers() for q in Queue.get_all_by_priority(job_model.queue_name)], []))
-    logger.info('Deleting %s (max date = %s, total: %d (planned: %d))', model_repr, max_date, count, len(planned))
+    logger.info('Deleting %s (limit: %s, max-date: %s, total: %d (planned: %d))', model_repr, max_delete, max_date, count, len(planned))
     kept = 0
     iterate = 0
-    while True:
+    while (not max_delete) or (iterate < max_delete):
         instances = list(job_model.collection().instances(skip_exist_test=True)[kept:kept+batch_size])
         if not instances:
             break
         for instance in instances:
             iterate += 1
             if iterate and not iterate % log_step:
-                logger.info('Deleted %d on %d (kept %d)', done, count, kept)
+                logger.info('Deleted %d on %d (limit %s, kept %d)', done, count, max_delete, kept)
             try:
                 if instance.ident in planned or keep_days and instance.added.hget()[:10] >= max_date:
                     # skip it, so stay in the list, so skip in the next batch
@@ -81,18 +84,20 @@ def clean_job_model(job_model, log_step=1000, keep_days=7, batch_size=100):
                 kept += 1
             else:
                 done += 1
+            if max_delete and iterate >= max_delete:
+                break
 
     for q in Queue.get_all_by_priority(job_model.queue_name):
         q.success.delete()
         q.errors.delete()
 
-    logger.info('Total %s deleted: %d on %d, keep %d (including %d errors and %d planned)', model_repr, done, count, kept, errors, len(planned))
+    logger.info('Total %s deleted (limit %s): %d on %d, keep %d (including %d errors and %d planned)', model_repr, max_delete, done, count, kept, errors, len(planned))
 
 
-def clean_all(log_step=1000, keep_days=7, batch_size=100):
+def clean_all(log_step=1000, keep_days=7, batch_size=100, max_delete=None):
     """
     Remove from the Error and all Job models entries older than the given number of days
     """
-    clean_errors(log_step=log_step, keep_days=keep_days, batch_size=batch_size)
+    clean_errors(log_step=log_step, keep_days=keep_days, batch_size=batch_size, max_delete=max_delete)
     for J in JobRegistry:
-        clean_job_model(J, log_step=log_step, keep_days=keep_days, batch_size=batch_size)
+        clean_job_model(J, log_step=log_step, keep_days=keep_days, batch_size=batch_size, max_delete=max_delete)
