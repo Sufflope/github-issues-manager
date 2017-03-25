621b67d86126205ba48b94d8d5bdb8ae29eec3d8
Stephane Angel (Twidi) <s.angel@twidi.com>
2017-03-25 03:25:13 +0100

    Make django-debug-toolbar work

--- gim/urls.py
+++ gim/urls.py
@@ -1,3 +1,4 @@
+from django.conf import settings
 from django.conf.urls import include, url
 
 from django.contrib import admin
@@ -11,3 +12,9 @@ urlpatterns = [
     url(r'^graphs/', include('gim.graphs.urls', namespace='graphs')),
     url(r'^', include('gim.front.urls', namespace='front', app_name='front')),
 ]
+
+if settings.DEBUG and settings.DEBUG_TOOLBAR:
+    import debug_toolbar
+    urlpatterns = [
+        url(r'^__debug__/', include(debug_toolbar.urls)),
+    ] + urlpatterns
